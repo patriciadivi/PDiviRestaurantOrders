51d7af63c33515f5db9053774a5f0cae834e05d0
PatriciaDivi <patriciavianad@gmail.com>
2023-01-12 00:57:41 -0300

    fix(BUGS): :bug: Correção de bugs

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,60 +1,61 @@
 import csv
 
+
 def accessOrders(path_to_file):
     if path_to_file.endswith(".csv"):
         try:
             with open(path_to_file, newline="") as filePath:
                 odersColumns = ['client', 'requests', 'day']
                 return list(csv.DictReader(filePath, fieldnames=odersColumns))
-        except:
+        except FileNotFoundError:
             raise FileNotFoundError(f"Arquivo inexistente:  '{path_to_file}'")
     raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
 
-def mostRequestedByMaria(resultOrders, nameClient):
-    resMaria = [eachLine['requests'] for eachLine in resultOrders if eachLine['client'] == nameClient]
+
+def mostRequestedByMaria(orders, client):
+    orders = [line['requests'] for line in orders if line['client'] == client]
 
     mostRequested = {}
-    for element in resMaria:
+    for element in orders:
         if element not in mostRequested:
             mostRequested[element] = 1
         mostRequested[element] += 1
 
     checkLargerOrder = max(mostRequested, key=mostRequested.get)
     return checkLargerOrder
-            
-def amountOfHamburgerByArnaldo(resultOrders, nameClient, nameRequest):
+
+
+def amountOfHamburger(resultOrders, nameClient, nameRequest):
     amountOfHamburger = []
-    for each_line in resultOrders:
-        if each_line['client'] == nameClient and each_line['requests'] == nameRequest:
-            amountOfHamburger.append(each_line)
+    for line in resultOrders:
+        if line['client'] == nameClient and line['requests'] == nameRequest:
+            amountOfHamburger.append(line)
             # print('', len(amountOfHamburger))
-    return len(amountOfHamburger)   
+    return len(amountOfHamburger)
 
-def orderNotPlaced(resultOrders, keyOfOrders, nameClient):
-    clientOfOrdersComplete = set([line[keyOfOrders] for line in resultOrders ])
-    clientOfOrders = set([line[keyOfOrders] for line in resultOrders if line['client'] == nameClient])
 
-    return clientOfOrdersComplete - clientOfOrders
+def orderNotPlaced(orders, key, nameCli):
+    cliOrdersComplete = set([line[key] for line in orders])
+    ordersC = set([line[key] for line in orders if line['client'] == nameCli])
+
+    return cliOrdersComplete - ordersC
 
 
 def analyze_log(path_to_file):
     resultOrders = accessOrders(path_to_file)
-    #Qual o prato mais pedido por 'maria'?
     resultOrderMaria = mostRequestedByMaria(resultOrders, "maria")
-    #Quantas vezes 'arnaldo' pediu 'hamburguer'?
-    resultAmountArnaldo = amountOfHamburgerByArnaldo(resultOrders, 'arnaldo', 'hamburguer')
-    #Quais pratos 'joao' nunca pediu?
-    resultRequestsOrderNotPlaced = orderNotPlaced(resultOrders, 'requests', 'joao')
-    #Quais dias 'joao' nunca foi à lanchonete?
-    resultDayOrderNotPlaced = orderNotPlaced(resultOrders, 'day', 'joao')
+    resultAmount = amountOfHamburger(resultOrders, 'arnaldo', 'hamburguer')
+    resultRequests = orderNotPlaced(resultOrders, 'requests', 'joao')
+    resultDay = orderNotPlaced(resultOrders, 'day', 'joao')
 
     with open("data/mkt_campaign.txt", "w") as file:
-                    file.write(
-                        f"{resultOrderMaria}\n"
-                        f"{resultAmountArnaldo}\n"
-                        f"{resultRequestsOrderNotPlaced}\n"
-                        f"{resultDayOrderNotPlaced}\n"
-                        )
+        file.write(
+                    f"{resultOrderMaria}\n"
+                    f"{resultAmount}\n"
+                    f"{resultRequests}\n"
+                    f"{resultDay}\n"
+                    )
+
 
 if __name__ == "__main__":
     analyze_log("data/orders_1.csv")
